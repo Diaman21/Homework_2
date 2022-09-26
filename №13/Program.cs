9b9086582fa8facf55eb31da298d312c020a7b58
Diaman21 <diaman21@mail.ru>
2022-09-26 17:00:58 +0300

    задача 13

---
+++ №13/Program.cs
@@ -0,0 +1,18 @@
+﻿        //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        public void task_13()
+        {
+            Console.WriteLine("ЗАДАЧА №13");
+            Console.WriteLine("Введите любое число: ");
+            int num = Math.Abs(Input());
+
+            string numTxt = Convert.ToString(num);
+            if (numTxt.Length > 2)
+            {
+                Console.WriteLine($"Третья цифра: {numTxt[2]}");
+            }
+            else
+            {
+                Console.WriteLine("Третьей цифры нет!");
+            }
+            BeautuLine();
+        }
