2f0f556502b180be567d27294ffd6603dc7ac3be
Diaman21 <diaman21@mail.ru>
2022-09-26 17:26:37 +0300

    задача 15

--- №15/Program.cs
+++ №15/Program.cs
@@ -1,68 +1,12 @@
 ﻿
         //Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-        public void task_15()
-        {
-            Console.WriteLine("ЗАДАЧА №15");
-            Console.Write("Введите номер дня недели:");
-            int num = Input();
-            if (num >=1 && num <=5)
-            {
-                Console.WriteLine($"{DayWeek(num)}! Это будний день!");
-            }
-            else if (num == 6 || num == 7)
-            {
-                Console.WriteLine($"{DayWeek(num)}! Это выходной день!");
-            }
-            else
-            {
-                Console.WriteLine("Введите значение от 1 до 7!");
-            }
-            BeautuLine();
-        }
 
-        static string DayWeek(int num)
-        {
-            string dayWeek = "";
-            if (num == 1)
-            {
-                dayWeek = "Понедельник";
-                return dayWeek;
-            }
-            else if (num == 2)
-            {
-                dayWeek = "Вторник";
-                return dayWeek;
-            }
-            else if (num == 3)
-            {
-                dayWeek = "Среда";
-                return dayWeek;
-            }
-            else if (num == 4)
-            {
-                dayWeek = "Четверг";
-                return dayWeek;
-            }
-            else if (num == 5)
-            {
-                dayWeek = "Пятница";
-                return dayWeek;
-            }
-            else if (num == 6)
-            {
-                dayWeek = "Суббота";
-                return dayWeek;
-            }
-            else if (num == 7)
-            {
-                dayWeek = "Воскресение";
-                return dayWeek;
-            }
-            else
-            {
-                dayWeek = "Введитет число от 1 до 7!!!";
-                return dayWeek;
-            }
-        }
-    }
+Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a <= 5)
+{    
+    Console.WriteLine("Будний день");
 }
+else
+    Console.WriteLine("Выходной");
