ae09cd5ec63e444b41112b3cffe1e9cb95467119
Diaman21 <diaman21@mail.ru>
2022-09-26 16:57:47 +0300

    задача 10

---
+++ №10/Program.cs
@@ -0,0 +1,17 @@
+﻿        public void task_10()
+        {
+            Console.WriteLine("ЗАДАЧА №10");
+            Console.WriteLine("Введите трехзначное число: ");
+            int num = Input();
+            if (num > 99 && num < 1000) 
+            { 
+            int two = num / 10 % 10;
+            Console.WriteLine($"Вторая цифра вашего число: {two}");
+            }
+            else
+            {
+                Console.WriteLine("Неправльная размерность числа");
+            }
+
+            BeautuLine();
+        }
